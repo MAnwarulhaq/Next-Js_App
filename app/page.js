2647d6402e39a82c8b8bc7a842f4f76a993f2744
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-09-24 15:43:37 +0500

    Navigation

--- app/page.js
+++ app/page.js
@@ -1,15 +1,23 @@
 'use client'
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 const Page = () => {
   const handleClick = () => {
    alert('Hy Anwar');
   }
+
+  const router=useRouter()
+
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <button onClick={() => alert('Button Clicked!')}>Click Me</button>
-      <button onClick={handleClick}>Log to Console</button>
+      {/* <button onClick={() => alert('Button Clicked!')}>Click Me</button>
+      <button onClick={handleClick}>Log to Console</button> */}
+      <Link href='/login'>Go to Login Page</Link>
+      <button onClick={()=>router.push('/login')}>Login</button>
     </div>
   )
 }
